8cb40d1bb9d2418e74f23637ed4df7953a28640c
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2021-12-13 20:10:57 -0600

    update homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,9 @@ import Layout from '@components/layout'
 export default function App() {
   return (
     <Layout>
-      <header className="flex flex-col text-5xl items-center">
-        <h1 className="text-purple-800">david</h1>
-        <h2 className="text-gray-700">magnuson</h2>
+      <header className="flex flex-col space-y-4 text-5xl items-center">
+        <span className="text-gray-500 text-6xl">hi, i'm</span>
+        <h1 className="text-purple-800">david magnuson</h1>
         <h3 className="text-gray-600 text-xl pt-2">
           davidjamesmagnuson@gmail.com
         </h3>
