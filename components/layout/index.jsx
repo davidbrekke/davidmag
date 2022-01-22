0b27f4fb267173e17612f1fe937f4ed224e3c404
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-01-22 17:54:52 -0600

    feat: add background gradient

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -8,12 +8,19 @@ const Layout = ({ children, title }) => (
       <meta description="davidmag" />
       <meta author="david magnuson" />
     </Head>
-
-    <main className="min-w-screen min-h-screen flex flex-col scroll items-center justify-evenly max-w-5xl m-auto p-4">
-      <Nav />
-      {children}
-    </main>
+    <Background>
+      <main className="min-w-screen min-h-screen flex flex-col scroll items-center justify-evenly max-w-5xl m-auto p-4">
+        <Nav />
+        {children}
+      </main>
+    </Background>
   </>
 )
 
 export default Layout
+
+const Background = ({ children }) => (
+  <div className="min-w-screen min-h-screen bg-gradient-to-tr from-indigo-300 via-red-200 to-blue-200">
+    {children}
+  </div>
+)
