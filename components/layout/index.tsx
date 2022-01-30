2fd2a40c64112c406bc3eb23fb4b7a92ef626428
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-01-30 13:09:26 -0600

    fix: remove meta tags causing error

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,15 +6,11 @@ const Layout = ({ children, title }) => (
   <>
     <Head>
       <title>davidmag{title && ` | ${title}`}</title>
-      <meta description="davidmag" />
-      <meta author="david magnuson" />
     </Head>
-    <Background>
-      <main className="min-w-screen min-h-screen flex flex-col scroll items-center justify-evenly max-w-5xl m-auto p-4">
-        <Nav />
-        {children}
-      </main>
-    </Background>
+    <Main>
+      <Nav />
+      {children}
+    </Main>
   </>
 )
 
@@ -24,4 +20,12 @@ const Background = ({ children }) => (
   <div className="min-w-screen min-h-screen bg-gradient-to-tr from-indigo-300 via-red-200 to-blue-200">
     {children}
   </div>
+)
+
+const Main = ({ children }) => (
+  <Background>
+    <main className="min-w-screen min-h-screen flex flex-col scroll items-center justify-evenly max-w-5xl m-auto p-4">
+      {children}
+    </main>
+  </Background>
 )
