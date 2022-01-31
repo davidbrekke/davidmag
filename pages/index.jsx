0fc96cae9688d03caa25403508a18a8ac6ee1af6
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-01-31 15:40:19 -0600

    animate content load

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,21 @@
 import Layout from '@components/layout'
 import AboutMe from '@components/AboutMe'
 import Connect from '@components/Connect'
+import Animate from '@components/Animate'
 
 export default function App() {
   return (
     <Layout>
-      <div className="flex flex-col space-y-4 items-center text-center">
-        <span className="text-gray-500 text-4xl">{`hi i'm`}</span>
-        <h1 className="text-purple-600 text-6xl">
-          <strong>david magnuson</strong>
-        </h1>
-        <AboutMe />
-        <Connect />
-      </div>
+      <Animate>
+        <div className="flex flex-col space-y-4 items-center text-center">
+          <span className="text-gray-500 text-4xl">{`hi i'm`}</span>
+          <h1 className="text-purple-600 text-6xl">
+            <strong>david magnuson</strong>
+          </h1>
+          <AboutMe />
+          <Connect />
+        </div>
+      </Animate>
     </Layout>
   )
 }
