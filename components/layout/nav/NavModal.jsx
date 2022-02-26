f86f703eac23d1e44fde807d94b6649343e6f8be
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-26 12:37:03 -0600

    update nav styling

--- components/layout/nav/NavModal.jsx
+++ components/layout/nav/NavModal.jsx
@@ -3,7 +3,7 @@ import NavModalLink from '@components/layout/nav/NavModalLink'
 
 const NavModal = ({ isOpen }) =>
   isOpen && (
-    <div className="flex flex-col items-center absolute text-2xl text-gray-500 top-24 bg-gray-100 right-6 py-2 px-4 shadow rounded-lg hover:shadow-lg transition">
+    <div className="flex flex-col items-center absolute text-2xl border border-opacity-50 text-gray-500 top-24 bg-white right-6 py-2 px-4 shadow-lg rounded-lg hover:shadow-xl transition">
       <NavItems navItems={navItems} />
     </div>
   )
