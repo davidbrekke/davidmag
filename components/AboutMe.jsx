d374c253d2ba6da686133158936f3b41bf8e5d7e
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-26 14:24:27 -0600

    update about me

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,56 +1,104 @@
+import Divider from '@components/Divider'
 const AboutMe = () => (
   <>
+    <Divider />
     <Education />
+    <Divider />
+    <Skills />
+    <Divider />
+    <Currently />
+    <Divider />
     <Interests />
-    <Learning />
+    <Divider />
     <Passion />
+    <Divider />
   </>
 )
 
 export default AboutMe
 
-const Learning = () => (
-  <p className="text-gray-500 text-2xl">
-    currently, i am learning{' '}
-    <Link href="https://www.typescriptlang.org/">typescript</Link>,{' '}
-    <Link href="https://soliditylang.org/">solidity</Link> & the{' '}
-    <Link href="https://ethereum.org/en/developers/docs/evm/">
-      ethereum virtual machine (EVM)
-    </Link>
-    {` as well as other 'web3'
-    technologies such as `}
-    <Link href="https://ipfs.io/">ipfs</Link>,{' '}
-    <Link href="https://thegraph.com/en/">graph protocol</Link>,{' '}
-    <Link href="https://ethereum.org/en/developers/docs/oracles/">oracles</Link>
-    {' & '}
-    <Link href="https://ethereum.org/en/developers/docs/scaling/layer-2-rollups/">
-      rollups
-    </Link>
-  </p>
+const Currently = () => (
+  <div className="flex flex-col items-center text-gray-500 text-2xl">
+    <p className="font-bold">currently</p>
+    <div className="flex flex-col">
+      <p>
+        <i>building</i> <Link href="http://test.2loud.xyz">2loud.xyz</Link>
+        <p className="text-lg">audio file sharing social application</p>
+        <p className="text-lg">
+          built with nextjs, typescript, tailwind & supabase
+        </p>
+      </p>
+      <Divider min />
+      <p className="">
+        <i>learning</i> typescript, solidity & the ethereum virtual machine
+        (EVM)
+        <p className="text-lg">
+          & other web3 technologies such as ipfs, graph protocol, oracles &
+          rollups
+        </p>
+      </p>
+    </div>
+  </div>
+)
+
+const Skills = () => (
+  <div className="flex flex-col items-center text-gray-500 text-2xl">
+    <p className="font-bold">skills & knowledge</p>
+    <div className="flex flex-col">
+      <p className="">
+        javascript & python
+        <p className="text-xl">solidity, swift, java, c#</p>
+      </p>
+      <Divider min />
+      <p>reactjs, nextjs, nodejs, flask</p>
+      <Divider min />
+      <p>rest, graphql, aws, azure, sql, mongo, orm</p>
+      <Divider min />
+      <p>css, css in js, utility classes (tailwind)</p>
+      <Divider min />
+      <p>ethersjs, web3js, hardhat, truffle</p>
+      <Divider min />
+      <p>git, figma, ui/ux design, tdd</p>
+    </div>
+  </div>
 )
 
 const Education = () => (
-  <p className="text-gray-500 text-2xl">
-    i am a recent software engineering graduate from{' '}
-    <Link href="https://dunwoody.edu/">dunwoody college of technology</Link>
-  </p>
+  <div className="flex flex-col items-center text-gray-500 text-3xl">
+    <p className="font-bold text-gray-500">software engineer</p>
+    <p className="text-gray-500 text-xl">
+      fall 2021 graduate of{' '}
+      <Link href="https://dunwoody.edu/">dunwoody college of technology</Link>
+    </p>
+    <p className="text-gray-500 text-lg">
+      <Link href="https://catalog.dunwoody.edu/catalog-student-handbook/academic-programs/engineering/software-engineering-seng-bachelor-science/#text">
+        bachelors of science in software engineering
+      </Link>
+    </p>
+  </div>
 )
 
 const Interests = () => (
-  <p className="text-gray-500 text-2xl">
-    {`i am interested in full stack engineering, front-end architecture,
-      blockchain & smart contracts, machine learning & data science,
+  <div className="flex flex-col items-center text-gray-500 text-2xl">
+    <p className="font-bold">interests</p>
+    <p className="text-gray-500 text-2xl">
+      {`full stack engineering, front-end architecture,
+      blockchain & smart contracts, machine learning & data science, ui/ux design,
       agricultural iot, 'smart homes' and basically anything related to
       technology & software`}
-  </p>
+    </p>
+  </div>
 )
 
 const Passion = () => (
-  <p className="text-gray-500 text-2xl">
-    my main passion & goal is to expand the reach of technology to the world
-    community so that everybody can benefit from the opportunities that
-    technology makes possible
-  </p>
+  <div className="flex flex-col items-center text-gray-500 text-2xl">
+    <p className="font-bold">passion</p>
+    <p className="text-gray-500 text-2xl">
+      my main passion & goal is to expand the reach of technology to the world
+      community so that everybody can benefit from the opportunities that
+      technology makes possible
+    </p>
+  </div>
 )
 
 const Link = ({ href, underline = true, children }) => (
