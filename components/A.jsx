3e0f7843956d08d738a83293c54d48e305fa46a0
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-26 14:18:47 -0600

    update A styling

--- components/A.jsx
+++ components/A.jsx
@@ -2,7 +2,7 @@ const A = ({ href, text }) => (
   <a
     target="_blank"
     rel="noreferrer"
-    className="px-4 py-1 rounded text-purple-600 hover:bg-purple-400 hover:text-gray-50 transition"
+    className="px-4 py-1 w-min rounded-lg text-purple-600 hover:bg-purple-400 hover:text-gray-50 transition"
     href={href}
   >
     {text}
